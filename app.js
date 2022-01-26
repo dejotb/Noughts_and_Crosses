0a0614b4aa4efb978da16e25343057e6c7732da6
dejotb <dejotb@gmail.com>
2022-01-26 19:42:30 +0100

    gitignore file added

--- app.js
+++ app.js
@@ -1,86 +1,79 @@
-'use strict'
-
 class Player {
-    constructor(image) {
-        this.image = image
-    }
+  constructor(player, image) {
+    this.player = player;
+    this.image = image;
+    this.allFieldsSelected = [];
+  }
 }
 
+const play = function () {
+  const boardGrid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
 
+  const board = document.querySelector('.board__container');
+  const boardField = document.querySelectorAll('.board__item');
 
+  // let fieldsSelected = ['', '', ''];
 
-const board = document.querySelector('.board__container')
-const boardField = document.querySelectorAll('.board__item')
+  const player1 = new Player('Player One', 'cross.svg');
+  const player2 = new Player('Player Two', 'circle.jpg');
 
+  const players = [player1, player2];
+  let roundNr = 0;
 
-let fieldsSelected = ['', '', ''];
-
-const player1 = new Player ('cross.svg');
-const player2 = new Player ('circle.jpg');
-
-const players = [player1, player2];
-let roundNr = 0
-
-
-const addImage = function(e, player) {
+  const addImage = function (e, player) {
     const html = `
-        <img src='img/${player.image}' alt='image'>
-    `;
+            <img src='img/${player.image}' alt='image'>
+        `;
     if (e.target.classList.contains('board__item'))
-    e.target.insertAdjacentHTML('afterbegin', html)
-}
+      e.target.insertAdjacentHTML('afterbegin', html);
+  };
 
-const addRound = function() {
+  const addRound = function () {
     return roundNr++;
-}
+  };
 
-const selectBoardField = function(e) {
+  const selectBoardField = function (e) {
     let activePlayer = players[0];
     if (roundNr % 2) {
-        activePlayer = players[1]
+      activePlayer = players[1];
     } else {
-        activePlayer = players[0]
-    };
-    if (e.target.classList.contains('board__item')) {
-        const selectedField = e.target.dataset.number;
-        fieldsSelected[0] = selectedField;
-        console.log(fieldsSelected);
-        addImage(e, activePlayer);
-        addRound()
-        console.log(roundNr)
+      activePlayer = players[0];
     }
+    if (e.target.classList.contains('board__item')) {
+      const fieldSelected = e.target.dataset.number;
+      activePlayer.allFieldsSelected.push(parseInt(fieldSelected));
+      console.log(activePlayer.allFieldsSelected);
+      addImage(e, activePlayer);
+      addRound();
+      console.log(roundNr);
+    }
+  };
 
-}
+  board.addEventListener('click', selectBoardField);
 
+  const playerOneSelections = [4, 6, 5];
 
-board.addEventListener('click', selectBoardField)
+  const containsAll = boardGrid.map((arr) =>
+    arr.every((el) => playerOneSelections.includes(el))
+  );
 
+  console.log(containsAll);
+  console.log(containsAll.some((el) => el === true));
 
+  // const containsAll = arr1.every(element => {
+  //     return arr2.includes(element);
+  // });
 
-const playerOneSelections = [4,1,5];
+  console.log(boardGrid);
+};
 
-const boardGrid = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7],
-    ];
-
-
-const containsAll = boardGrid.map(arr => arr.every(el => {
-    return playerOneSelections.includes(el);
-}));
-
-console.log(containsAll);
-console.log(containsAll.some(el => el === true));
-
-// const containsAll = arr1.every(element => {
-//     return arr2.includes(element);
-// });
-
-
-console.log(boardGrid);
+play();
