622ed3e4c7622e153aa0a06473ea61db439f73cc
dejotb <dejotb@gmail.com>
2022-08-25 17:27:04 +0200

    implemented modules

---
+++ data/DOM-elements.js
@@ -0,0 +1,24 @@
+import Player from '../lib/player.js';
+
+// Players
+
+export const player1 = new Player('Player1', 'cross.svg', 1);
+export const player2 = new Player('Player2', 'circle.svg', 2);
+
+// Select board classes
+
+export const board = document.querySelector('.board__container');
+export const boardFields = document.querySelectorAll('.board__item');
+export const playersScore = document.querySelectorAll('.score__total');
+export const modal = document.querySelector('.modal__container');
+
+// Select button id
+
+export const btnReset = document.querySelector('#btn__reset-score');
+
+// Select Referee classes
+
+export const mouthOpen = document.querySelector('.mouth__open');
+export const eyes = document.querySelectorAll('.eyes');
+export const handLeft = document.querySelector('.hand__left');
+export const handRight = document.querySelector('.hand__right');
---
+++ data/elements.js
@@ -0,0 +1,18 @@
+import { player1, player2 } from './DOM-elements.js';
+
+// A grid of all winning combinations
+
+export const boardGridWinCombinations = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+// An array of players
+
+export const players = [player1, player2];
